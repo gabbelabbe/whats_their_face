0e835febb98558067f37a0db533c4660db2bb736
Gabriel <gabbe.stark@gmail.com>
2019-08-22 09:48:19 +0200

    Nu funkar det lilla fina saken :)

--- game.rb
+++ game.rb
@@ -1,9 +1,8 @@
+require 'sqlite3'
 require_relative 'models/db_objects.rb'
 require_relative 'models/students.rb'
 require_relative 'models/group.rb'
-require_relative 'app'
-require 'sqlite3'
 
 students = Student.all() { {join: "group_name"} }
 
-puts students
+p students
